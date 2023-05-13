89b8f7237ee130646b8cc12c20e4ad357c0ff9dc
Happy1353 <79153940348@yandex.ru>
2023-05-13 02:05:28 +0200

    add mock wallet

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import {SafeAreaView, Text, View} from 'react-native';
+import {SafeAreaView, Text, View, Image} from 'react-native';
 
 import {Choose} from '../../components/Choose/Choose';
 
 import styles from './Home.scss';
+import avatar from '../../assets/avatar.jpg';
+import ETHERSVG from '../../assets/etherSmall.svg';
 
 export const Home = () => {
+  //TODO: connect wallet
   return (
     <SafeAreaView style={styles.background}>
       <View style={{paddingHorizontal: 16}}>
-        <View>
-          <View></View>
-          <Text>12.3 ETH</Text>
+        <View style={[styles.walletContainer, {marginBottom: 16}]}>
+          <View style={styles.walletContainer}>
+            <View style={styles.walletEtherContainer}>
+              <ETHERSVG />
+            </View>
+            <Text style={styles.walletMoney}>12.3 ETH</Text>
+          </View>
+          <Image source={avatar} style={styles.walletAvatar} />
         </View>
         <Choose />
       </View>
